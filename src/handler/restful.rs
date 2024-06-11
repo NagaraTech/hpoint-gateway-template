066162d3100f1bdd99cc4c7c2a201ea8636bcae6
rongbai <1577973940@qq.com>
2024-06-11 16:33:47 +0800

    modify restful

--- src/handler/restful.rs
+++ src/handler/restful.rs
@@ -4,24 +4,27 @@ use crate::db::connection::get_db_conn;
 use crate::data::task::{PostTaskData,PostTaskResponse};
 use crate::db::entities::post_data;
 use sea_orm::entity::*;
-
-
+use serde_json::value::Value;
+use chrono::{DateTime, TimeZone, Utc, Local};
+use sea_orm::prelude::DateTimeWithTimeZone;
 
 
 pub async fn handle_event_post(Json(payload): Json<PostTaskData>) -> Result<Json<PostTaskResponse>, StatusCode> {
     // format!("Received field1: {}, field2: {}", payload.field1, payload.field2)
     let db_conn = get_db_conn().await;
 
+    let json_value: Value = serde_json::to_value(payload.data).unwrap();
+    let datetime_utc: DateTime<Utc> = Utc.timestamp(payload.timestamp as i64, 0);
     if payload.address.len() > 0 {
         let post_task = post_data::ActiveModel {
             id: NotSet,
-            project: Default::default(),
-            event_type: Default::default(),
-            address: Default::default(),
-            timestamp: Default::default(),
-            sign_method: Default::default(),
-            sign: Default::default(),
-            data: Default::default(),
+            project: ActiveValue::Set(payload.project),
+            event_type: ActiveValue::Set(payload.event_type),
+            address: ActiveValue::Set(payload.address),
+            timestamp: ActiveValue::Set(DateTimeWithTimeZone::from(datetime_utc)),
+            sign_method: ActiveValue::Set(payload.sign_method),
+            sign: ActiveValue::Set(payload.sign),
+            data: ActiveValue::Set(Option::from(json_value)),
         };
         post_task.insert(db_conn).await.expect("Fail To Insert Post Data");
         Ok(Json(PostTaskResponse{
